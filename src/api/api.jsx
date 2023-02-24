5ae81769f50c5af52b7127e7248997ed822d7342
fabranx <107358529+fabranx@users.noreply.github.com>
2023-02-24 11:01:48 +0100

    change git limit

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -9,6 +9,6 @@ const clientkey = import.meta.env.VITE_CLIENTKEY
 
 export const getGifs = async ({queryKey, pageParam=''}) => {
   const [_key, keyword ] = queryKey
-  const response = await axios.get(`${address}search?q=${keyword}&key=${apikey}&client_key=${clientkey}&limit=10&locale=it&pos=${pageParam}`)
+  const response = await axios.get(`${address}search?q=${keyword}&key=${apikey}&client_key=${clientkey}&limit=20&locale=it&pos=${pageParam}`)
   return response.data      
 }
